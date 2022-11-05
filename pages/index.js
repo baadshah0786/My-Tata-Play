b694875180d4b74e538b943a54b57799a912defa
Baadshah0786 <87961897+baadshah0786@users.noreply.github.com>
2022-11-05 10:34:19 +0530

    fgf

--- pages/index.js
+++ pages/index.js
@@ -135,129 +135,129 @@ export default function Home() {
                 <Grid.Column computer={8} tablet={12} mobile={16}>
                   <Segment loading={loading}>
                     <Form>
-                    <img alt="TataPlay" src="https://ik.imagekit.io/techiesneh/Tata_Play_2022_logo.svg_ptaIQ8TXe.png" className="center" width="260" height="40"></img>
+                      <img alt="TataPlay" src="https://ik.imagekit.io/techiesneh/Tata_Play_2022_logo.svg_ptaIQ8TXe.png" className="center" width="260" height="40"></img>
                       <Form.Group inline>
-                       
-                      <text>{"\n"}</text>
-                      <div className='center'>   
-                        <label>Login via:</label>
+
+                        <text>{"\n"}</text>
+                        <div className='center'>
+                          <label>Login via:</label>
                         </div>
-                        <div className='center'>   
-                        <Form.Field>
-                                               <Radio
-                            
-                            label='OTP'
-                            name='loginTypeRadio'
-                            value='OTP'
-                            checked={loginType === 'OTP'}
-                            onChange={(e, { value }) => { setLoginType(value); }}
-                          />
-                          
-                        </Form.Field>
+                        <div className='center'>
+                          <Form.Field>
+                            <Radio
+
+                              label='OTP'
+                              name='loginTypeRadio'
+                              value='OTP'
+                              checked={loginType === 'OTP'}
+                              onChange={(e, { value }) => { setLoginType(value); }}
+                            />
+
+                          </Form.Field>
                         </div>
-                        <div className='center'>   
-                        <Form.Field>
-                          <Radio
-                            label='Password'
-                            name='loginTypeRadio'
-                            value='PWD'
-                            checked={loginType === 'PWD'}
-                            onChange={(e, { value }) => { setLoginType(value); }}
-                          />
-                        </Form.Field>
-                        <div className='center'>   
-                      </Form.Group>
+                        <div className='center'>
+                          <Form.Field>
+                            <Radio
+                              label='Password'
+                              name='loginTypeRadio'
+                              value='PWD'
+                              checked={loginType === 'PWD'}
+                              onChange={(e, { value }) => { setLoginType(value); }}
+                            />
+                          </Form.Field>
+                          </div>
+                          </Form.Group>
 
-                      {
-                        loginType === 'OTP' ?
-                          <>
-                            <Form.Field disabled={otpSent}>
-                              <label>RMN</label>
-                              <input value={rmn} placeholder='Registered Mobile Number' onChange={(e) => setRmn(e.currentTarget.value)} />
-                            </Form.Field>
-                            <Form.Field disabled={otpSent}>
-                              <label>Subscriber ID</label>
-                              <input value={sid} placeholder='Subscriber ID' onChange={(e) => setSid(e.currentTarget.value)} />
-                            </Form.Field>
-                            <Form.Field disabled={!otpSent}>
-                              <label>OTP</label>
-                              <input value={otp} placeholder='OTP' onChange={(e) => setOtp(e.currentTarget.value)} />
-                            </Form.Field>
-                            {
-                              otpSent ? <Button primary onClick={authenticateUser}>Login</Button> :
-                                <Button primary onClick={getOTP}>Get OTP</Button>
-                            }
-                          </>
-                          :
-                          <>
-                            <Form.Field>
-                              <label>Subscriber ID</label>
-                              <input value={sid} placeholder='Subscriber ID' onChange={(e) => setSid(e.currentTarget.value)} />
-                            </Form.Field>
-                            <Form.Field>
-                              <label>Password</label>
-                              <input type='password' value={pwd} placeholder='Password' onChange={(e) => setPwd(e.currentTarget.value)} />
-                            </Form.Field>
-                            <Button primary onClick={authenticateUser}>Login</Button>
-                          </>
-                      }
+                          {
+                            loginType === 'OTP' ?
+                              <>
+                                <Form.Field disabled={otpSent}>
+                                  <label>RMN</label>
+                                  <input value={rmn} placeholder='Registered Mobile Number' onChange={(e) => setRmn(e.currentTarget.value)} />
+                                </Form.Field>
+                                <Form.Field disabled={otpSent}>
+                                  <label>Subscriber ID</label>
+                                  <input value={sid} placeholder='Subscriber ID' onChange={(e) => setSid(e.currentTarget.value)} />
+                                </Form.Field>
+                                <Form.Field disabled={!otpSent}>
+                                  <label>OTP</label>
+                                  <input value={otp} placeholder='OTP' onChange={(e) => setOtp(e.currentTarget.value)} />
+                                </Form.Field>
+                                {
+                                  otpSent ? <Button primary onClick={authenticateUser}>Login</Button> :
+                                    <Button primary onClick={getOTP}>Get OTP</Button>
+                                }
+                              </>
+                              :
+                              <>
+                                <Form.Field>
+                                  <label>Subscriber ID</label>
+                                  <input value={sid} placeholder='Subscriber ID' onChange={(e) => setSid(e.currentTarget.value)} />
+                                </Form.Field>
+                                <Form.Field>
+                                  <label>Password</label>
+                                  <input type='password' value={pwd} placeholder='Password' onChange={(e) => setPwd(e.currentTarget.value)} />
+                                </Form.Field>
+                                <Button primary onClick={authenticateUser}>Login</Button>
+                              </>
+                          }
 
-                    </Form>
-                  </Segment>
-                </Grid.Column>
-                <Grid.Column></Grid.Column>
-              </Grid.Row> :
-              <Grid.Row>
-                <Grid.Column></Grid.Column>
-                <Grid.Column computer={8} tablet={12} mobile={16}>
-                  <Segment loading={loading}>
-                    <Header as="h1">Welcome, {theUser.sName}</Header>
-                    {
-                      theUser !== null && theUser.acStatus !== "DEACTIVATED" ?
-                        <Message>
-                          <Message.Header>Dynamic URL to get m3u: </Message.Header>
-                          {/* <Image centered src={'https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=' + encodeURIComponent(m3uMeta.url)} size='small' /> */}
-                          <p>
-                            <a href={dynamicUrl}>{dynamicUrl}</a>
-                          </p>
-                          <p>
-                            You can use the above m3u URL in OTT Navigator or Tivimate app to watch all your subscribed channels.
-                          </p>
-                          <p>
-                            The generated m3u URL is for permanent use and is not required to be refreshed every 24 hours. Enjoy!
-                          </p>
-                        </Message>
-                        :
-                        <Header as='h3' style={{ color: 'red' }}>Your Tata Sky Connection is deactivated.</Header>
-                    }
+                        </Form>
+                      </Segment>
+                    </Grid.Column>
+                    <Grid.Column></Grid.Column>
+                  </Grid.Row> :
+                  <Grid.Row>
+                    <Grid.Column></Grid.Column>
+                    <Grid.Column computer={8} tablet={12} mobile={16}>
+                      <Segment loading={loading}>
+                        <Header as="h1">Welcome, {theUser.sName}</Header>
+                        {
+                          theUser !== null && theUser.acStatus !== "DEACTIVATED" ?
+                            <Message>
+                              <Message.Header>Dynamic URL to get m3u: </Message.Header>
+                              {/* <Image centered src={'https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=' + encodeURIComponent(m3uMeta.url)} size='small' /> */}
+                              <p>
+                                <a href={dynamicUrl}>{dynamicUrl}</a>
+                              </p>
+                              <p>
+                                You can use the above m3u URL in OTT Navigator or Tivimate app to watch all your subscribed channels.
+                              </p>
+                              <p>
+                                The generated m3u URL is for permanent use and is not required to be refreshed every 24 hours. Enjoy!
+                              </p>
+                            </Message>
+                            :
+                            <Header as='h3' style={{ color: 'red' }}>Your Tata Sky Connection is deactivated.</Header>
+                        }
 
-                    <Button negative onClick={logout}>Logout</Button>
-                  </Segment>
-                </Grid.Column>
-                <Grid.Column></Grid.Column>
-              </Grid.Row>
+                        <Button negative onClick={logout}>Logout</Button>
+                      </Segment>
+                    </Grid.Column>
+                    <Grid.Column></Grid.Column>
+                  </Grid.Row>
           }
-          <Grid.Row style={{ display: err === '' ? 'none' : 'block' }}>
-            <Grid.Column></Grid.Column>
-            <Grid.Column computer={8} tablet={12} mobile={16}>
-              <Message color='red'>
-                <Message.Header>Error</Message.Header>
-                <p>
-                  {err}
-                </p>
-              </Message>
-            </Grid.Column>
-            <Grid.Column></Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column></Grid.Column>
-            <Grid.Column textAlign='center' computer={8} tablet={12} mobile={16}>
-              <a href="https://github.com/baadshah0786/My-Tata-Play" target="_blank" rel="noreferrer">View source code on Github</a>
-            </Grid.Column>
-            <Grid.Column></Grid.Column>
-          </Grid.Row>
-        </Grid>
+                  <Grid.Row style={{ display: err === '' ? 'none' : 'block' }}>
+                    <Grid.Column></Grid.Column>
+                    <Grid.Column computer={8} tablet={12} mobile={16}>
+                      <Message color='red'>
+                        <Message.Header>Error</Message.Header>
+                        <p>
+                          {err}
+                        </p>
+                      </Message>
+                    </Grid.Column>
+                    <Grid.Column></Grid.Column>
+                  </Grid.Row>
+                  <Grid.Row>
+                    <Grid.Column></Grid.Column>
+                    <Grid.Column textAlign='center' computer={8} tablet={12} mobile={16}>
+                      <a href="https://github.com/baadshah0786/My-Tata-Play" target="_blank" rel="noreferrer">View source code on Github</a>
+                    </Grid.Column>
+                    <Grid.Column></Grid.Column>
+                  </Grid.Row>
+                </Grid>
       }
-    </div>
+              </div>
   )
 }
